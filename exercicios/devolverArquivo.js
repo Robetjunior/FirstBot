234f2f707abf171e18ee8e2ea04723b373c120c4
Jose Roberto <juniorferreirarob@gmail.com>
2020-09-10 14:34:25 -0300

    create devolverArquivos.js

--- exercicios/devolverArquivo.js
+++ exercicios/devolverArquivo.js
@@ -3,4 +3,18 @@ const Telegraf = require("telegraf");
 const axios = require("axios");
 const bot = new Telegraf(env.token);
 
-bot.on("voice", async (ctx) => {});
+// o bot envia uma MENSAGEM DE VOZ. A mesma que o usuario acabou de enviar no chat
+bot.on("voice", async (ctx) => {
+  const id = ctx.update.message.voice.file_id;
+  const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`);
+  ctx.replyWithVoice({ url: `${env.apiFileUrl}/${res.data.result.file_path}` });
+});
+
+// o bot envia uma FOTO. A mesma que o usuario acabou de enviar no chat
+bot.on("photo", async (ctx) => {
+  const id = ctx.update.message.photo[0].file_id;
+  const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`);
+  ctx.replyWithPhoto({ url: `${env.apiFileUrl}/${res.data.result.file_path}` });
+});
+
+bot.startPolling();
