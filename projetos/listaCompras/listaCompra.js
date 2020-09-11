8d970e6066c715f9e07fc38b46757ae22721f5c6
Jose Roberto <juniorferreirarob@gmail.com>
2020-09-11 20:22:34 -0300

    create new folder listaCompras

---
+++ projetos/listaCompras/listaCompra.js
@@ -0,0 +1,5 @@
+const env = require("../../.env");
+const Telegraf = require("telegraf");
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
+const bot = new Telegraf(env.token);
