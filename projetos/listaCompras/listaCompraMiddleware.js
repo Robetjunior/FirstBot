711c7f0ea61a280a0a79221f8b520f45c0121c6a
Jose Roberto <juniorferreirarob@gmail.com>
2020-09-11 22:20:33 -0300

    update listaCompraMiddleware.js

--- projetos/listaCompras/listaCompraMiddleware.js
+++ projetos/listaCompras/listaCompraMiddleware.js
@@ -16,7 +16,27 @@ const botoes = (lista) =>
 
 bot.use(session());
 
-bot.start(async (ctx) => {
+const verificarUsuario = (ctx, next) => {
+  //verificar se e o mesmo ID vindo de uma mensagem
+  // o ID pode vir por uma callback ou pela mensagem
+  const mesmoIDMsg =
+    ctx.update.message && ctx.update.message.from.id === env.userID;
+  const mesmoIDCallback =
+    ctx.update.callback_query &&
+    ctx.update.callback_query.from.id === env.userID;
+
+  if (mesmoIDMsg || mesmoIDCallback) {
+    next();
+  } else {
+    ctx.reply(`Desculpe, nao fui autorizado a conversar com voce.`);
+  }
+};
+
+// Voce pode ter mais de um middleware..
+const processando = ({ reply }, next) =>
+  reply("processando...").then(() => next());
+
+bot.start(verificarUsuario, async (ctx) => {
   console.log(ctx.update.message.from);
   const name = ctx.update.message.from.first_name;
   await ctx.reply(`Seja bem vindo, ${name}!`);
@@ -24,13 +44,13 @@ bot.start(async (ctx) => {
   ctx.session.lista = [];
 });
 
-bot.on("text", (ctx) => {
+bot.on("text", verificarUsuario, processando, (ctx) => {
   let msg = ctx.update.message.text;
   ctx.session.lista.push(msg);
   ctx.reply(`${msg} adicionado`, botoes(ctx.session.lista));
 });
 
-bot.action(/delete (.+)/, (ctx) => {
+bot.action(/delete (.+)/, verificarUsuario, processando, (ctx) => {
   ctx.session.lista = ctx.session.lista.filter((item) => item !== ctx.match[1]);
   ctx.reply(`${ctx.match[1]} deletado com sucesso!`, botoes(ctx.session.lista));
 });
