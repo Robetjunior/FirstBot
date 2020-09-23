57b195591a79397235920cf6b1647bd6f889ef1a
Jose Roberto <juniorferreirarob@gmail.com>
2020-09-23 14:40:50 -0300

    update listaComprasGrupo

--- projetos/listaCompras/listaComprasGrupo.js
+++ projetos/listaCompras/listaComprasGrupo.js
@@ -5,3 +5,19 @@ const Markup = require("telegraf/markup");
 const bot = new Telegraf(env.token);
 
 let dados = {};
+
+const gerarBotoes = (lista) =>
+  Extra.markup(
+    Markup.inlineKeyboard(
+      lista.map((item) => Markup.callbackButton(item, `delete ${item}`)),
+      { columns: 3 }
+    )
+  );
+
+bot.start(async (ctx) => {
+  const name = ctx.update.message.from.first_name;
+  await ctx.reply(`Seja bem vindo, ${name}!`);
+  await ctx.reply(`Escreva os itens que voce deseja adicionar!...`);
+});
+
+bot.use((ctx, next) => {});
